434d68713495c62c3f06d8e2424d93f556ccb979
LawrenceKyalo <lawrencekkyalo@gmail.com>
2025-05-15 11:57:27 +0300

    initializing default.py

---
+++ default.py
@@ -0,0 +1,24 @@
+class MyClass:
+    """
+    A simple Python class example.
+    """
+
+    def __init__(self, attribute):
+        """
+        Initialize the class with an attribute.
+        :param attribute: Any value to initialize the class attribute.
+        """
+        self.attribute = attribute
+
+    def get_attribute(self):
+        """
+        Returns the value of the attribute.
+        """
+        return self.attribute
+
+    def set_attribute(self, value):
+        """
+        Sets the value of the attribute.
+        :param value: The new value for the attribute.
+        """
+        self.attribute = value
