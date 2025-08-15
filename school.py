2b327e7137647aa3a3f98de445dadfcd0ed5bac6
Lawrence Kyalo <lawrencekkyalo@gmail.com>
2025-08-15 11:48:07 +0300

    python strings refresh

--- school.py
+++ school.py
@@ -7,9 +7,16 @@ def is_valid_email(email):
         return "Invalid email: too short"
     if '@' not in email or '.' not in email:
         return "Invalid email: missing '@' or '.'"
+    if email.startswith('@') or email.startswith('.'):
+        return "Invalid email: cannot start with '@' or '.'"
+    if email.endswith('@') or email.endswith('.'):
+        return "Invalid email: cannot end with '@' or '.'"  
     if email.count('@') > 1:
         return "Invalid email: too many '@'"
     if email.count('.') > 1:
         return "Invalid email: too many '.'"
     return "Valid email"
+
+result = is_valid_email(email)
+print(result)
 
