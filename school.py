5484c8226506b12d3ce1034bffa04abdf35a23a7
Lawrence Kyalo <lawrencekkyalo@gmail.com>
2025-08-15 11:42:56 +0300

    python strings refresh

--- school.py
+++ school.py
@@ -1,5 +1,15 @@
-let = "python strings refresh"
-if 'ring' not in let:
-    print('Substring not found')
-else:
-    print('...')
+#lawrencekyalo09@gmail.com
+#get user input check forlength, @ and . return valid or invalid email
+
+email = input("Enter your email:")
+def is_valid_email(email):
+    if len(email) < 5:
+        return "Invalid email: too short"
+    if '@' not in email or '.' not in email:
+        return "Invalid email: missing '@' or '.'"
+    if email.count('@') > 1:
+        return "Invalid email: too many '@'"
+    if email.count('.') > 1:
+        return "Invalid email: too many '.'"
+    return "Valid email"
+
